e3c10749bc862b9b6df5033681608749db1e6537
asick <coltonburki@gmail.com>
2021-09-12 04:50:53 -0500

    new theme, threaded and non-blocking tasks

--- src/main.py
+++ src/main.py
@@ -2,9 +2,24 @@
 # https://github.com/asickwav/video-compressor
 # Python 3.9.7 64-bit
 
-import platform, os, subprocess, sys, psutil, asyncio, webbrowser, threading, time
+import platform, os, subprocess, psutil, webbrowser, threading
 import PySimpleGUI as sg
 import utils as Utils
+
+sg.LOOK_AND_FEEL_TABLE["swag"] = {
+    'BACKGROUND': 'white',
+    'TEXT': 'black',
+    'INPUT': 'white',
+    'TEXT_INPUT': '#000000',
+    'SCROLL': 'white',
+    'BUTTON': ('white', '#2b3340'),
+    'PROGRESS': ('#01826B', '#D0D0D0'),
+    'BORDER': 1,
+    'SLIDER_DEPTH': 0,
+    'PROGRESS_DEPTH': 0
+}
+
+sg.theme("swag")
 
 class Main:
     def __init__(self) -> None:
@@ -17,7 +32,6 @@ class Main:
         self.compressing = False
         self.trimming = False
         self.trimmed = False
-        self.process_started = False
         
         # UI Preferences
         self.font_large = "Arial 16 bold"
@@ -44,8 +58,10 @@ class Main:
         self.cur_video_path = ""
         self.cur_video_extension = ""
         self.cur_queue = 0
+        self.cur_pass = 1
         self.proc = ""
-        self.ffmpeg_is_valid = Utils.validate_ffmpeg()
+        self.ffmpeg_is_valid = False
+        self.ffmpeg_path = Utils.get_ffmpeg_path()
         
         self.layout = [
             [sg.Text("%s Video Compressor %s" % (self.author, self.version), font=self.font_large)],
@@ -54,7 +70,7 @@ class Main:
             [sg.Text("Audio Bitrate", font=self.font_small), sg.Input(self.audio_bitrate, font=self.font_tiny, key="audio_bitrate", size=(7, 1)), sg.Text("Kbps")],
             [sg.Text("Framerate", font=self.font_small), sg.Input(self.fps, font=self.font_tiny, key="framerate", size=(7, 1)), sg.Text("FPS")],
             [sg.Text("File Extension", font=self.font_small), sg.Input(self.file_extension, font=self.font_tiny, key="extension", size=(7, 1))],
-            [sg.Text("Trim", font=self.font_small), sg.Input(self.trim_s, font=self.font_tiny, key="trim_start", size=(7, 1)), sg.Input(self.trim_e, font=self.font_tiny, key="trim_end", size=(7, 1))],
+            [sg.Text("Trim (hh:mm:ss)", font=self.font_small), sg.Input(self.trim_s, font=self.font_tiny, key="trim_start", size=(7, 1)), sg.Input(self.trim_e, font=self.font_tiny, key="trim_end", size=(7, 1))],
             [sg.Text("Enable Trim", font=self.font_small), sg.Checkbox("", font=self.font_tiny, default=False, key="trim", change_submits=True)],
             [sg.Text("Remove Audio", font=self.font_small), sg.Checkbox("", font=self.font_tiny, default=False, key="mute", change_submits=True)],
             [sg.Text("Use H.265 Codec", font=self.font_small), sg.Checkbox("", font=self.font_tiny, default=False, key="h265", change_submits=True)],
@@ -65,26 +81,46 @@ class Main:
             [sg.Button("Start", key="start", size=(8, 1)), sg.Button("Abort", key="abort", size=(8, 1)), sg.Button("Help", key="help", size=(8, 1)), sg.Button("Github", key="github", size=(8, 1))],
             [sg.Text("")],
             [sg.HorizontalSeparator(pad=None)],
-            [sg.Text(f"Created by {self.author} with Python", font=self.font_tiny)],
+            [sg.Text("This app is free and open-source!", font=self.font_tiny)],
+            [sg.Button("Support me on Ko-fi", key="support")],
         ]
         
-        if not self.ffmpeg_is_valid:
-            x = threading.Thread(target=Utils.install_ffmpeg)
-            x.start()
-        
         self.main()
+        
+    def start(self, values) -> None:
+        if not self.compressing and not self.trimming:
+            self.apply_options(values)
+            self.selected_videos = Utils.list_videos(values["select_videos"])
+            
+            if len(self.selected_videos) > 0:
+                if not self.ffmpeg_is_valid:
+                    print("Please wait for FFmpeg to finish installing.")
+                else:
+                    if self.enable_trim and not self.trimmed:
+                        x = threading.Thread(target=self.trim)
+                        x.start()
+                    else:
+                        x = threading.Thread(target=self.compress)
+                        x.start()
+            else:
+                print('No videos selected!')
+            
+            if not self.ffmpeg_is_valid:
+                print("Please wait for FFmpeg to finish installing.")
+        else:
+            print("Compression started, click Abort to stop.")
     
     def help(self) -> None:
-        sg.popup("""
-            Resolution: Set your video's dimensions (width x height).
-            \n\nTarget File Size: Set the targetted file size for your video.
-            \n\nAudio Bitrate: Set your video's audio bitrate.
-            \n\nFramerate: Set your video's fps.
-            \n\nFile Extension: Set the file type of your video.
-            \n\nTrim: The first box is your video's initial start time, the second box is the duration afterwards.
-            \n\nEnable Trim: Must be enabled to trim your video.
+        sg.popup(
+            """Resolution: Set your video's dimensions (width x height).
+            \n\nTarget File Size: Set the desired file size for your video(s) in MB.
+            \n\nAudio Bitrate: Set the audio bitrate for your video(s) in Kbps.
+            \n\nFramerate: Set the fps for your video(s).
+            \n\nFile Extension: Set the file type of your video(s).
+            \n\nTrim: The first box is the time where your video will start, the second box is how long your video will be. (This is just how FFmpeg handles trimming, I hate it too.)
+            \n\nEnable Trim: Must be enabled to trim your video(s).
             \n\nRemove Audio: Self explanitory.
-            \n\nUse H.265 Codec: Set your video to use the H.265 codec for a higher quality output. If you plan on sharing your video on Discord then do NOT use this option, it will not embed.
+            \n\nUse H.265 Codec: Set your video(s) to use the H.265 codec for a higher visual quality. If you plan on sharing your video on Discord then do NOT use this option, it will not embed.
             \n\nPortrait Mode: Flips the set width and height resolution to output verticle videos correctly. If you've already manually set the resolution to vertical dimensions then this option is ignored.
         """)
     
@@ -94,36 +130,158 @@ class Main:
                 p = psutil.Process(proc.pid)
                 p.kill()
 
-                try:
-                    os.remove("TEMP")
-                    print("Cleaned up temp files.")
-                except:
-                    pass
-
                 self.compressing = False
                 self.trimming = False
                 self.trimmed = False
                 self.process_started = False
+        
+        self.cleanup()
+        print("Aborted.")
+    
+    def apply_options(self, values) -> None:
+        self.res_w = int(values["res_w"])
+        self.res_h = int(values["res_h"])
+        print("Set resolution to %sx%s." % (self.res_w, self.res_h))
+        
+        self.target_file_size = int(values["file_size"])
+        print("Set target file size to %sMB." % self.target_file_size)
+        
+        self.audio_bitrate = int(values["audio_bitrate"])
+        print("Set audio bitrate to %sk." % self.audio_bitrate)
+        
+        self.fps = int(values["framerate"])
+        print("Set framerate to %s FPS." % self.fps)
+        
+        self.file_extension = "." + values["extension"]
+        print("Set file extension to %s." % self.file_extension)
+        
+        self.trim_s = values["trim_start"]
+        self.trim_e = values["trim_end"]
+        self.enable_trim = values["trim"]
+        self.remove_audio = values["mute"]
+        
+        if self.remove_audio:
+            print("Muting video(s).")
+            
+        self.use_h265 = values["h265"]
+        
+        if self.use_h265:
+            print("Using H.265 codec.")
+            
+        self.portrait_mode = values["portrait"]
+        
+        if self.portrait_mode:
+            if self.res_h < self.res_w:
+                h = self.res_h
+                self.res_h = self.res_w
+                self.res_w = h
+                print("Using portrait mode, switching resolution.")
+            else:
+                print("Using portrait mode.")
+    
+    def cleanup(self) -> None:
+        try:
+            os.remove("TEMP")
+            print("Cleaned up temp files.")
+        except:
+            print("No files to clean.")
+    
+    def trim(self) -> None:
+        if not self.trimming:
+            self.cur_video = self.selected_videos[self.cur_queue]
+            self.cur_video_trimmed = '"%s%s-trimmed%s"' % (Utils.get_video_path(self.cur_video), Utils.get_video_name(self.cur_video), self.file_extension)
+            input = '"%s"' % self.cur_video
+            trim_start = "%s" % self.trim_s
+            trim_end = "%s" % self.trim_e
+            output = self.cur_video_trimmed
+            cmd = "%s -y -ss %s -i %s -t %s -c copy %s" % (self.ffmpeg_path, trim_start, input, trim_end, output)
+            print(cmd)
 
-                print("Aborting.")
+            self.proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            self.trimming = True
+            print("Trimming video %s/%s..." % (self.cur_queue + 1, len(self.selected_videos)))
+            
+        while self.trimming:
+            stdout, stderr = self.proc.communicate()
+
+            if self.proc.returncode == 0:
+                self.trimming = False
+                self.trimmed = True
+                print("Trimming complete, starting compression...")
+                self.compress()
+            else:
+                self.trimming = False
+                self.trimmed = False
+                print("Trimming failed, aborting...")
+                self.abort()
+    
+    def compress(self) -> None:
+        if not self.compressing:
+            self.cur_video = self.selected_videos[self.cur_queue]
+            
+            if self.trimmed:
+                self.cur_video = self.cur_video_trimmed
+                print("Using trimmed version: %s" % self.cur_video_trimmed)
+            
+            input = '"%s"' % self.cur_video
+            codec = "libx265" if self.use_h265 else "libx264"
+            video_bitrate = Utils.calculate_video_bitrate(self.cur_video, self.target_file_size, self.audio_bitrate)
+            audio_bitrate = "-b:a %sk" % self.audio_bitrate if not self.remove_audio else "-an"
+            fps = "%s" % self.fps
+            resolution = "%s:%s" % (self.res_w, self.res_h)
+            output = '"%s%s-compressed%s"' % (Utils.get_video_path(self.cur_video), Utils.get_video_name(self.cur_video), self.file_extension)
+            
+            pass_1 = "%s -i %s -y -c:v %s -b:v %sk -r %s -vf scale=%s -pass 1 -an -f mp4 TEMP" % (
+                self.ffmpeg_path, input, codec, video_bitrate, fps, resolution
+            )
+            
+            pass_2 = "%s -y -i %s -y -c:v %s -b:v %sk -r %s -vf scale=%s -pass 2 -c:a aac %s %s" % (
+                self.ffmpeg_path, input, codec, video_bitrate, fps, resolution, audio_bitrate, output
+            )
+            
+            if self.cur_pass == 1:
+                self.proc = subprocess.Popen(pass_1, stdout=subprocess.PIPE)
+            else:
+                self.proc = subprocess.Popen(pass_2, stdout=subprocess.PIPE)
+                
+            self.compressing = True
+            print("Compressing video %s/%s, Pass %s/2..." % (self.cur_queue + 1, len(self.selected_videos), self.cur_pass))
+        
+        while self.compressing:
+            stdout, stderr = self.proc.communicate()
+
+            if self.proc.returncode == 0:
+                self.compressing = False
+                
+                if self.cur_pass == 1:
+                    self.cur_pass = 2
+                    self.compress()
+                else:
+                    print("Video %s/%s compressed!\n" % (self.cur_queue + 1, len(self.selected_videos)))
+                    self.cur_pass = 1
+                    
+                    if (self.cur_queue + 1 < len(self.selected_videos)):
+                        self.cur_queue += 1
+                        self.compress()
+                    else:
+                        print('Job done!\n')
+            else:
+                self.compressing = False
+                print("Compression failed, aborting...")
+                self.abort()
     
     def main(self) -> None:
-        self.window = sg.Window("%s Video Compressor" % self.author, self.layout, resizable=False, finalize=True, element_justification="center")
+        self.window = sg.Window("%s Video Compressor" % self.author, self.layout, element_justification="center")
         
         while True:
-            event, values, = self.window.read(timeout=1)
+            event, values = self.window.read(timeout=1)
 
             if event in (sg.WIN_CLOSED, "Exit", "Cancel"):
                 self.abort()
-                sys.exit()
+                quit()
 
             if event == "start":
-                if not self.trimming and not self.compressing:
-                    self.selected_videos = Utils.list_videos(values["select_videos"])
-                    self.apply_options(values)
-                    self.start()
-                else:
-                    print("Compression already begun. Click Abort if you need to restart.")
+                self.start(values)
             
             if event == "abort":
                 self.abort()
@@ -133,6 +291,11 @@ class Main:
             
             if event == "github":
                 webbrowser.open("https://github.com/asickwav/video-compressor")
+            
+            if not self.ffmpeg_is_valid:
+                if not Utils.ffmpeg_is_downloading:
+                    x = threading.Thread(target=Utils.install_ffmpeg)
+                    x.start()
         
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,13 @@
-import platform, os, subprocess, fnmatch, shutil, requests, sys, zipfile, time
+# Created by asick
+# https://github.com/asickwav/video-compressor
+# Python 3.9.7 64-bit
+
+import platform, os, subprocess, fnmatch, shutil, requests, sys
 
 ffmpeg_download_link = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
 ffmpeg_download_path = "%s/downloads/ffmpeg-release-essentials.zip" % os.getcwd()
 ffmpeg_path = "%s/ffmpeg" % os.getcwd()
+ffmpeg_is_downloading = False
 ffmpeg_downloaded = False
 ffmpeg_installed = False
 
@@ -23,12 +28,14 @@ def validate_ffmpeg() -> bool:
             print("FFmpeg validated!")
             return True
 
-    print("Couldn't locate FFmpeg.")
+    print("Couldn't locate FFmpeg, will download.")
+    install_ffmpeg()
     return False
 
 def install_ffmpeg() -> None:
     global ffmpeg_installed
     global ffmpeg_downloaded
+    global ffmpeg_is_downloading
     
     if platform.system() == "Windows":
         if not os.path.exists("%s/ffmpeg" % os.getcwd()):
@@ -42,10 +49,11 @@ def install_ffmpeg() -> None:
         if os.path.isfile("%s/ffmpeg.exe" % ffmpeg_path):
             ffmpeg_installed = True
             print("Found ffmpeg!")
-        else:            
+        else:
+            ffmpeg_is_downloading = True
+            
             with open(ffmpeg_download_path, "wb") as f:
                 print("Downloading ffmpeg to %s" % ffmpeg_download_path)
-                print("Please wait...")
                 response = requests.get(ffmpeg_download_link, stream=True)
                 total_length = response.headers.get('content-length')
 
@@ -58,11 +66,12 @@ def install_ffmpeg() -> None:
                     for data in response.iter_content(chunk_size=4096):
                         dl += len(data)
                         f.write(data)
-                        done = int(50 * dl / total_length)
-                        sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
+                        progress_percent = (dl / total_length) * 100
+                        print("Downloading FFmpeg: %s\n" % progress_percent)
                         sys.stdout.flush()
                     
             ffmpeg_downloaded = True
+            ffmpeg_is_downloading = False
             print("\n")
             unzip_ffmpeg()
 
@@ -71,6 +80,18 @@ def unzip_ffmpeg() -> None:
     shutil.unpack_archive(ffmpeg_download_path, ffmpeg_path)
     print("Unzipped ffmpeg contents.")
     move_files()
+
+def get_ffmpeg_path() -> str:
+    if platform.system() == "Windows":
+        return "%s/ffmpeg/ffmpeg.exe" % os.getcwd()
+    else:
+        return "ffmpeg"
+
+def get_ffprobe_path() -> str:
+    if platform.system() == "Windows":
+        return "%s/ffmpeg/ffprobe.exe" % os.getcwd()
+    else:
+        return "ffprobe"
 
 def move_files() -> None:
     global ffmpeg_installed
@@ -88,3 +109,46 @@ def get_files() -> None:
     for path, dirlist, filelist in os.walk(os.getcwd() + "/ffmpeg"):
         for name in fnmatch.filter(filelist, "*.exe"):
             yield os.path.join(path, name)
+
+def list_videos(videos) -> list:
+    if videos != "":
+        return videos.split(';')
+    else:
+        return []
+
+def calculate_video_bitrate(video, target_file_size, audio_bitrate) -> float:
+    video_duration = get_video_duration(video)
+
+    if video_duration:
+        magic = round(((target_file_size * 8192.0) / (1.048576 * video_duration) - audio_bitrate))
+        return magic
+    else:
+        return None
+
+def get_video_duration(video) -> float:
+    try:
+        video = '"%s"' % video
+        cmd = "%s -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 %s" % (get_ffprobe_path(), video)
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        return float(proc.stdout.readline())
+    except:
+        print("Couldn't get video duration!")
+        print("If you trimmed this video try different trim settings.")
+        return None
+
+def get_video_name(video) -> str:
+    name_with_extension = os.path.basename(video)
+    split = name_with_extension.split('.')
+    just_name = name_with_extension.replace('.' + split[-1], '')
+    return just_name
+
+def get_video_extension(video) -> str:
+    name_with_extension = os.path.basename(video)
+    split = name_with_extension.split('.')
+    just_extension = '.' + split[-1]
+    return just_extension
+
+def get_video_path(video) -> str:
+    name_with_extension = os.path.basename(video)
+    just_path = video.replace(name_with_extension, '')
+    return just_path
